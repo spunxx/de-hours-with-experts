365e218cbb77754951a16b38e18e73b36144d9e9
Ryan Suther <rsuther@aegis-strategies.com>
2020-01-11 15:19:25 -0600

    fixes, pom tweak

--- java/src/main/java/com/labs1904/NextBiggestNumber.java
+++ java/src/main/java/com/labs1904/NextBiggestNumber.java
@@ -1,5 +1,7 @@
 package com.labs1904;
 
+import java.util.Arrays;
+
 public class NextBiggestNumber {
 
     public static void main(String[] args) {
@@ -10,7 +12,54 @@ public class NextBiggestNumber {
     }
 
     public static int getNextBiggestNumber(Integer i) {
-        //TODO: IMPLEMENT ME!
-        return 0;
+        
+        int n;
+        char []charArray = toCharArray(i);
+        int len = charArray.length;
+
+        for( n = len - 1; n > 0; n--) {
+            if (charArray[n] > charArray[n -1]) {
+                break;
+            }
+        }
+
+        if (n == 0) {
+            return -1;
+        } 
+
+        int x = charArray[n-1], min = n;
+
+        for(int j = n + 1; j < len; j++) {
+            if (charArray[j] > x && charArray[j] < charArray[min]) {
+                min = j;
+            }
+        }
+
+        swap(charArray, n - 1, min);
+        Arrays.sort(charArray, n, len);
+
+        return Integer.parseInt(new String(charArray));
     }
+
+
+    public static char[] toCharArray(Integer i) {
+
+        String temp = Integer.toString(i);
+        char[] charArray = new char[temp.length()];
+
+        for (int c = 0; c < temp.length(); c++) {
+            charArray[c] = temp.charAt(c);
+        }
+
+        return charArray;
+    }
+
+    public static void swap(char ar[], int i, int j) {
+        char temp = ar[i];
+        ar[i] = ar[j];
+        ar[j] = temp;
+    }
+
+
+    
 }
